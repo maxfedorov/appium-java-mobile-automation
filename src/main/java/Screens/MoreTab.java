8ca4c6760a2242fba28d1705cf85f404c70b3310
maxfedorov <maksfedorov@mail.ru>
2021-06-08 04:19:17 +0300

    Optimize imports

--- src/main/java/Screens/MoreTab.java
+++ src/main/java/Screens/MoreTab.java
@@ -2,7 +2,6 @@ package Screens;
 
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
-import org.openqa.selenium.WebDriver;
 
 public class MoreTab extends ScreenBase {
     public MoreTab(AndroidDriver<MobileElement> driver) {
--- src/main/java/Screens/SavedTab.java
+++ src/main/java/Screens/SavedTab.java
@@ -2,11 +2,6 @@ package Screens;
 
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-
-import static io.qameta.allure.Allure.step;
 
 public class SavedTab extends ScreenBase {
 
